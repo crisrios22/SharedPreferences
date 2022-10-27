fea566b7c14ef101221a4697436d8d52ef31f18c
cristian_rios <c.rios37@alumnos.santotomas.cl>
2022-10-27 01:56:57 -0300

    datos persistentes listo

--- app/src/main/java/com/demh/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/demh/sharedpreferences/MainActivity.java
@@ -2,13 +2,35 @@ package com.demh.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.txt_mail);
+
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        et1.setText(preferences.getString("mail",""));
     }
+
+    //metodo para el boton guardar
+    public void Guardar (View view) {
+        SharedPreferences preferencias = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor Obj_editor = preferencias.edit();
+        Obj_editor.putString("mail", et1.getText().toString());
+        Obj_editor.commit();
+        finish();
+
+    }
+
 }
